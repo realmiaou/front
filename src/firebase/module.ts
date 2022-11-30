ee029d813423d54d1abdd00c0dfe8c832af4eb37
Thibaut Mottet <thibaut.mottet@gmail.com>
2022-11-30 23:09:53 +0700

    fix(firestore): remove easy firestore

--- src/firebase/module.ts
+++ src/firebase/module.ts
@@ -1,49 +1,5 @@
-import { IEasyFirestoreModule } from 'vuex-easy-firestore/types/declarations'
-import { VuexModule } from 'vuex-class-component/dist/interfaces'
 import { createModule, extractVuexModule } from 'vuex-class-component'
-import { deserializeFirestoreDate } from './serializer'
 
-export const NuxtEasyVuexModule = <D extends { id: any }>(
-  namespaced: string,
-  configuration: Omit<
-        IEasyFirestoreModule,
-        'moduleName' | 'statePropName' | 'firestoreRefType'
-        >
-) =>
-    class extends VuexModule {
-      dataById: { [key in string]: D } = {}
-
-      public static readonly configuration = {
-        ...configuration,
-        moduleName: namespaced,
-        statePropName: 'dataById',
-        firestoreRefType: 'collection',
-        serverChange: {
-          addedHook: (updateStore: any, doc: any) =>
-            updateStore(deserializeFirestoreDate(doc)),
-          modifiedHook: (updateStore: any, doc: any) =>
-            updateStore(deserializeFirestoreDate(doc)),
-          removedHook: (updateStore: any, doc: any) =>
-            updateStore(deserializeFirestoreDate(doc))
-        }
-      }
-
-      async openDBChannel () {
-        await this.$store.dispatch(`${namespaced}/openDBChannel`)
-      }
-
-      fetchById (id: D['id']) {
-        return this.$store.dispatch(`${namespaced}/fetchById`, id)
-      }
-
-      async fetchAndAdd (request: any) {
-        const data = await this.$store.dispatch(
-                `${namespaced}/fetchAndAdd`,
-                request
-        )
-        return data.docs.map((doc: any) => deserializeFirestoreDate(doc.data()))
-      }
-    }
 export const NuxtVuexModule = (namespaced: string) =>
   createModule({ target: 'nuxt', namespaced })
 
