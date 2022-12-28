a4e6ce240de5f46287310c07e96b1d754c52cbd7
Thibaut Mottet <thibaut.mottet@gmail.com>
2022-12-28 15:25:04 +0000

    feat(module): New vuex module for realtime collection
    BREAKING CHANGE: This is a breaking change.

--- src/firebase/module.ts
+++ src/firebase/module.ts
@@ -1,7 +1,78 @@
 import { createModule, extractVuexModule } from 'vuex-class-component'
+import {
+  collection,
+  onSnapshot,
+  query,
+  QueryConstraint,
+  Unsubscribe
+} from 'firebase/firestore'
+import _ from 'lodash'
+import { deserializeFirestoreDate } from './serializer'
 
 export const NuxtVuexModule = (namespaced: string) =>
   createModule({ target: 'nuxt', namespaced })
+
+export const NuxtEasyVuexModule = <D extends { id: string | number }>(
+  namespaced: string,
+  collectionPath: string,
+  ...defaultQuery: QueryConstraint[]
+) => {
+  return class extends NuxtVuexModule(namespaced) {
+    private innerData: D[] = []
+    private unsub = null as Unsubscribe | null
+
+    protected get data () {
+      return _.chain(this.innerData)
+        .uniqBy(({ id }) => id)
+        .value()
+    }
+
+    openChannel (...queryConstraints: QueryConstraint[]) {
+      this.unsub = onSnapshot(
+        query(
+          collection(this.$store.$firestore, collectionPath),
+          ...defaultQuery,
+          ...queryConstraints
+        ),
+        (snapshot) => {
+          snapshot.docChanges().forEach((change) => {
+            if (change.type === 'added') {
+              this.innerData = [
+                deserializeFirestoreDate(change.doc.data()),
+                ...this.innerData
+              ]
+            }
+            if (change.type === 'modified') {
+              this.innerData = _.chain(this.innerData)
+                .map(entity =>
+                  entity.id === change.doc.data().id
+                    ? deserializeFirestoreDate(change.doc.data())
+                    : entity
+                )
+                .value()
+            }
+            if (change.type === 'removed') {
+              this.innerData = _.chain(this.innerData)
+                .remove(({ id }) => id !== change.doc.data().id)
+                .value()
+            }
+          })
+        }
+      )
+      return Promise.resolve()
+    }
+
+    closeChannel () {
+      if (this.unsub) {
+        this.unsub()
+      }
+    }
+
+    fetchById (id: D['id']) {
+      return this.innerData.find(entity => entity.id === id)!
+    }
+  }
+}
 
 export const extractModule = (module: any) => ({
   ...Object.values(extractVuexModule(module))[0],
