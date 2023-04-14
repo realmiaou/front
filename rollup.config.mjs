6b0db146406cb388e5c57eef599f26f04e0da5ce
Thibaut Mottet <thibaut.mottet@gmail.com>
2023-04-14 20:07:30 +0200

    fix(compile): use rollup

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,13 +1,15 @@
 import typescript from '@rollup/plugin-typescript'
 import commonjs from '@rollup/plugin-commonjs'
 import resolve from '@rollup/plugin-node-resolve'
+import terser from '@rollup/plugin-terser';
+
 
 export default {
   input: 'src/index.ts',
   output: {
     dir: 'lib',
-    format: 'es'
+    format: 'cjs'
   },
   external: ['firebase/functions', 'firebase/firestore', 'vuex-class-component'],
-  plugins: [typescript(), commonjs(), resolve()]
+  plugins: [typescript(), commonjs(), resolve(), terser()]
 }
